2774b86a95730248996446ba2ea30596c3835970
VolodymyrMatviiv <59965490+VolodymyrMatviiv@users.noreply.github.comgit config --global user.email 59965490+VolodymyrMatviiv@users.noreply.github.com>
2020-05-04 13:59:47 +0300

    added Faculty and User domain model

--- SelectionCommittee/src/main/java/ua/lviv/lgs/domain/Faculty.java
+++ SelectionCommittee/src/main/java/ua/lviv/lgs/domain/Faculty.java
@@ -2,10 +2,122 @@ package ua.lviv.lgs.domain;
 
 import java.util.List;
 
+import javax.persistence.Column;
+import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "faculty")
 public class Faculty {
-	
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Integer id;
+	
+	@Column(name = "faculty_name")
+	private FacultyName name;
+	
+	@Column(name = "qty_of_students")
 	private int qtyOfStudents;
+	
+	@ElementCollection
 	private List<Subjects> subjects;
+
+	public Faculty() {
+	}
+
+	public Faculty(int qtyOfStudents, FacultyName name, List<Subjects> subjects) {
+
+		this.qtyOfStudents = qtyOfStudents;
+		this.name = name;
+		this.subjects = subjects;
+	}
+
+	public Faculty(Integer id, int qtyOfStudents, FacultyName name, List<Subjects> subjects) {
+		super();
+		this.id = id;
+		this.qtyOfStudents = qtyOfStudents;
+		this.name = name;
+		this.subjects = subjects;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public int getQtyOfStudents() {
+		return qtyOfStudents;
+	}
+
+	public void setQtyOfStudents(int qtyOfStudents) {
+		this.qtyOfStudents = qtyOfStudents;
+	}
+
+	public FacultyName getName() {
+		return name;
+	}
+
+	public void setName(FacultyName name) {
+		this.name = name;
+	}
+
+	public List<Subjects> getSubjects() {
+		return subjects;
+	}
+
+	public void setSubjects(List<Subjects> subjects) {
+		this.subjects = subjects;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + qtyOfStudents;
+		result = prime * result + ((subjects == null) ? 0 : subjects.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Faculty other = (Faculty) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (name != other.name)
+			return false;
+		if (qtyOfStudents != other.qtyOfStudents)
+			return false;
+		if (subjects == null) {
+			if (other.subjects != null)
+				return false;
+		} else if (!subjects.equals(other.subjects))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Faculty [id=" + id + ", qtyOfStudents=" + qtyOfStudents + ", name=" + name + ", subjects=" + subjects
+				+ "]";
+	}
 
 }
--- SelectionCommittee/src/main/java/ua/lviv/lgs/domain/User.java
+++ SelectionCommittee/src/main/java/ua/lviv/lgs/domain/User.java
@@ -1,11 +1,185 @@
 package ua.lviv.lgs.domain;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Transient;
+
+@Entity
+@Table(name = "users")
 public class User {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Integer id;
+	
+	@Column(name = "name")
 	private String firstName;
+	
+	@Column(name = "surname")
 	private String lastName;
+	
+	@Column
+	private String email;
+	
+	@Column
 	private String password;
+	
+	@Transient
+	private String passwordConfirm;
+	
+	@Enumerated(EnumType.STRING)
 	private UserRole role;
+
+	public User() {
+	}
+
+	public User(User user) {
+		this.id = user.id;
+		this.firstName = user.firstName;
+		this.lastName = user.lastName;
+		this.password = user.password;
+		this.role = user.role;
+	}
+
+	public User(String firstName, String lastName, String email, String password, UserRole role) {
+
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+		this.password = password;
+		this.role = role;
+	}
+
+	public User(Integer id, String firstName, String lastName, String email, String password, String passwordConfirm,
+			UserRole role) {
+
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+		this.password = password;
+		this.passwordConfirm = passwordConfirm;
+		this.role = role;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public UserRole getRole() {
+		return role;
+	}
+
+	public void setRole(UserRole role) {
+		this.role = role;
+	}
+
+	public String getPasswordConfirm() {
+		return passwordConfirm;
+	}
+
+	public void setPasswordConfirm(String passwordConfirm) {
+		this.passwordConfirm = passwordConfirm;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((firstName == null) ? 0 : firstName.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((lastName == null) ? 0 : lastName.hashCode());
+		result = prime * result + ((password == null) ? 0 : password.hashCode());
+		result = prime * result + ((passwordConfirm == null) ? 0 : passwordConfirm.hashCode());
+		result = prime * result + ((role == null) ? 0 : role.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		if (firstName == null) {
+			if (other.firstName != null)
+				return false;
+		} else if (!firstName.equals(other.firstName))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (lastName == null) {
+			if (other.lastName != null)
+				return false;
+		} else if (!lastName.equals(other.lastName))
+			return false;
+		if (password == null) {
+			if (other.password != null)
+				return false;
+		} else if (!password.equals(other.password))
+			return false;
+		if (passwordConfirm == null) {
+			if (other.passwordConfirm != null)
+				return false;
+		} else if (!passwordConfirm.equals(other.passwordConfirm))
+			return false;
+		if (role != other.role)
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", password=" + password
+				+ ", role=" + role + "]";
+	}
 
 }
